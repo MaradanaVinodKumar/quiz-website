73144c6f6906057a27c0f29ab52f0749d075e478
vinod kumar <vinodkumarmaradana2514@gmail.com>
2023-10-16 10:57:38 +0530

    main

--- home_quize .js
+++ home_quize .js
@@ -6,7 +6,8 @@ function loads(id){
         data.json()
         .then((dat)=>{
                 //console.log(JSON.stringify(dat[0].Ques));
-            
+            if(dat.length>id+1)
+            {
             code+=`<div id="s-no${id}" class="main">
             <div id="Qes">
                 <div>
@@ -43,6 +44,47 @@ function loads(id){
             }
             bool=false;
             current=id;
+            }
+            else{
+                /*code=`
+                    <div id="end"> 
+                        <button id="Submit">Submit</button>           
+                    </div>`;*/
+                    code+=`<div id="s-no${id}" class="main">
+                    <div id="Qes">
+                        <div>
+                        <span id="s-no">${dat[id].se_no}.</span>
+                        </div>
+                    
+                        <div id="Qs_bg">
+                        <p id="Qs">${dat[id].Ques}</p>
+                        </div>
+                    </div>
+                        <div id="option">`;
+
+             for(x of dat[id].options)
+             {
+                code+=`<input type="radio" name="ANS${id}" value="1"><label>${x}</label><br>`
+             } 
+              code+= ` </div>
+                <div id="buttons"> 
+                    <div><button id="prev" onclick="prev()"><i class="fa fa-arrow-left" aria-hidden="true"></i>Prev </button></div>
+                    <div><button id="Submit">Submit</button></div>
+                </div>
+            </div>`;
+            document.getElementById('body').innerHTML=code;
+            if(id!=0){
+                document.getElementById(`s-no${id-1}`).style.display="none";
+            }
+            for(var x=1;x<=id-1;x++)
+            {
+                document.getElementById(`s-no${x-1}`).style.display="none";
+
+            }
+            bool=false;
+            current=id;
+            
+            }
         })
     })    
    
@@ -67,20 +109,16 @@ function next(){
     {
         bool=false;
     } 
-   
-   
    if(bool)
     {   
         pos++;
         document.getElementById(`s-no${pos-1}`).style.display="none";
-        document.getElementById(`s-no${pos}`).style.display="block";
-        
+        document.getElementById(`s-no${pos}`).style.display="block"; 
     }   
     else
     {
         pos++;
         loads(pos);
-
     }
 
 }
